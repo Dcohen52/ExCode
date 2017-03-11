21bc3fee6443c6a63aae024af5a8736ba2309345
Dekel Cohen <dcohen52@gmail.com>
2017-03-11 20:26:46 +0200

    Hihihi

--- ExCode1/ViewController.swift
+++ ExCode1/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+     var tapCount = 0
+    @IBAction func btnPressed(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = ("You pressed 10 times!")
+        }
+    }
+    @IBAction func coolBTN(_ sender: Any) {
+        theLabel.text = "Buttons are Cool!"
+        print("coolBTN tapped")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
